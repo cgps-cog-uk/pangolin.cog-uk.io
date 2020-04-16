4daac22302e45b6e0f47cc299adb5bd37a1d809d
Khalil <ka10@sanger.ac.uk>
2020-04-16 14:22:34 +0100

    Update DataGrid.vue

--- components/DataGrid.vue
+++ components/DataGrid.vue
@@ -121,7 +121,7 @@ export default {
   top: 64px;
   left: 0;
   right: 0;
-  bottom: 50px;
+  bottom: 48px;
   background-color: #fff;
   box-shadow: 0 12px 18px 2px rgba(34,0,51,.04),0 6px 22px 4px rgba(7,48,114,.12),0 6px 10px -4px rgba(14,13,26,.12);
   padding: 8px;
