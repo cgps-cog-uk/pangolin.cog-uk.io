6ea58d119aff9e3d260d5b69c871aa6ca2a10a9b
Anthony Underwood <au3@sanger.ac.uk>
2020-04-25 16:28:13 +0100

    remove unnecessary LOC

--- api/routes/process/submit.js
+++ api/routes/process/submit.js
@@ -1,11 +1,7 @@
 /* eslint-disable camelcase */
 /* eslint "no-throw-literal": 0 */
 
-const crypto = require("crypto");
-
-// const processFasta = require("../../utils/process-fasta");
 const queue = require("../../utils/queue");
-// const processSequence = require("../../utils/process-sequence");
 
 module.exports = function (req, res, next) {
   console.info("Got request to process a fasta sequence for", req.user.username);
